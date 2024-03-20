aa645c271e21516274825343df249cdc207cf125
Hugo Abonizio <hugo.abonizio@gmail.com>
2024-03-20 17:46:42 +0000

    Format

--- maritalk/download.py
+++ maritalk/download.py
@@ -2,20 +2,34 @@ import os
 import argparse
 from .resources.local import download, check_gpu, find_libs
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Start MariTalk Local server.")
-    parser.add_argument('--license', type=str, required=True, help='License key for downloading the model.')
-    parser.add_argument('--path', type=str, default='~/bin/maritalk', help='Path to save the executable.')
-    parser.add_argument('--cuda', type=int, choices=[11, 12], default=None, help='Installed CUDA version.')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download MariTalk Local server.")
+    parser.add_argument(
+        "--license",
+        type=str,
+        required=True,
+        help="License key for downloading the model.",
+    )
+    parser.add_argument(
+        "--path",
+        type=str,
+        default="~/bin/maritalk",
+        help="Path to save the executable.",
+    )
+    parser.add_argument(
+        "--cuda",
+        type=int,
+        choices=[11, 12],
+        default=None,
+        help="Installed CUDA version.",
+    )
     args = parser.parse_args()
 
     if not args.cuda:
         check_gpu()
     detected_versions = find_libs()
 
-    dependencies = {
-        "cuda_version": args.cuda or detected_versions["cuda_version"]
-    }
+    dependencies = {"cuda_version": args.cuda or detected_versions["cuda_version"]}
 
     if dependencies["cuda_version"] is None:
         raise Exception(
